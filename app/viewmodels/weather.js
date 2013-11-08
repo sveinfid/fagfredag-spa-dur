141ea069ebb4e34b5ba0e176acb2b2a95ce3fc4d
Svein Fidjestøl <donotreply@example.com>
2013-11-08 16:21:35 +0100

    Forecast for Oslo even more completed

--- app/viewmodels/weather.js
+++ app/viewmodels/weather.js
@@ -9,6 +9,15 @@
                 viewModel.location = ko.computed(function () {
                     return 'Været for ' + viewModel.forecast().place + ', ' + viewModel.forecast().country;
                 });
+                viewModel.currentTemperature = ko.computed(function () {
+                    return viewModel.forecast().forecasts[0].periods[0].temperature + '°';
+                });
+                viewModel.precipitation = ko.computed(function () {
+                    return viewModel.forecast().forecasts[0].periods[0].precipitationMin + ' – ' + viewModel.forecast().forecasts[0].periods[0].precipitationMax + ' mm';
+                });
+                viewModel.wind = ko.computed(function () {
+                    return viewModel.forecast().forecasts[0].periods[0].windType + ', ' + viewModel.forecast().forecasts[0].periods[0].windSpeed + ' fra ' + viewModel.forecast().forecasts[0].periods[0].windDirection;
+                });
             });
     };
     return viewModel;
